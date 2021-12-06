389cf7bc1a0268faea6d6057962c631734d22818
SolanaEngineer <topstack2021a@gmail.com>
2021-12-06 11:36:26 +0300

    add/unit tests

--- tests/atlas_vesting.ts
+++ tests/atlas_vesting.ts
@@ -1,17 +1,175 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { StablePool } from '../target/types/stable_pool';
+import { AtlasVesting } from '../target/types/atlas_vesting';
+import {Token } from "@solana/spl-token";
 
-describe('ratio', () => {
+const GLOBAL_STATE_TAG = "golbal-state-seed";
+const VESTING_TAG = "vesting-seed";
+
+const TOKEN_PROGRAM_ID = new anchor.web3.PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA');
+const RENT_SYSVAR_ID = new anchor.web3.PublicKey('SysvarRent111111111111111111111111111111111');
+const CLOCK_SYSVAR_ID = new anchor.web3.PublicKey('SysvarC1ock11111111111111111111111111111111');
+const SYSTEM_PROGRAM_ID = new anchor.web3.PublicKey('11111111111111111111111111111111');
+
+describe('atlas_vesting', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.StablePool as Program<StablePool>;
+  const program = anchor.workspace.AtlasVesting as Program<AtlasVesting>;
+  const wallet = program.provider.wallet;
+  const destinationOwner = anchor.web3.Keypair.generate();
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const tx = await program.rpc.initialize({});
-    console.log("Your transaction signature", tx);
+  it('Create global state', async () => {
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    const tx = await program.rpc.createGlobalState(globalStateKeyNonce, {
+      accounts: {
+        superOwner: wallet.publicKey,
+        globalState: globalStateKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID
+      }
+    });
+    console.log('txid = ', tx);
   });
+
+  let vestingTokenMint: Token = null;
+  
+  const currentTime = Date.now() / 1000;
+  const vestingStartTime = currentTime+30;
+  const vestingEndTime = vestingStartTime + 600;
+  const newVestingEndTime = vestingStartTime + 700;
+  it('Create Vesting', async () => {
+    vestingTokenMint = await Token.createMint(program.provider.connection, wallet as any, wallet.publicKey, wallet.publicKey, 9, program.programId);
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    let [vestingKey, vestingKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), wallet.publicKey.toBuffer(), ], program.programId);
+    let [vestingPoolKey, vestingPoolKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), vestingKey.toBuffer(), ], program.programId);
+    const tx = await program.rpc.createVesting(
+      globalStateKeyNonce,
+      vestingKeyNonce,
+      vestingPoolKeyNonce,
+      new anchor.BN(vestingStartTime),
+      new anchor.BN(vestingEndTime),
+      {
+      accounts: {
+        superOwner: wallet.publicKey,
+        vesting: vestingKey,
+        globalState: globalStateKey,
+        destinationOwner: destinationOwner.publicKey,
+        mintVestingToken: vestingTokenMint.publicKey,
+        poolVestingToken: vestingPoolKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID
+      }
+    });
+    console.log('txid = ', tx);
+  });
+  it('Update Vesting', async () => {
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    let [vestingKey, vestingKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), wallet.publicKey.toBuffer(), ], program.programId);
+    const tx = await program.rpc.updateVesting(
+      globalStateKeyNonce,
+      vestingKeyNonce,
+      new anchor.BN(vestingStartTime),
+      new anchor.BN(newVestingEndTime),
+      {
+      accounts: {
+        superOwner: wallet.publicKey,
+        vesting: vestingKey,
+        globalState: globalStateKey,
+        destinationOwner: destinationOwner.publicKey,
+        mintVestingToken: vestingTokenMint.publicKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID
+      }
+    });
+    console.log('txid = ', tx);
+  });
+  let userVestingToken: anchor.web3.PublicKey = null;
+  const vestingAmount = new anchor.BN(10 * 1000000000);
+  it('Deposit Vesting', async () => {
+    userVestingToken = await vestingTokenMint.createAccount(wallet.publicKey);
+    await vestingTokenMint.mintTo(userVestingToken, wallet as any, [], vestingAmount.toNumber());
+
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    let [vestingKey, vestingKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), wallet.publicKey.toBuffer(), ], program.programId);
+    let [vestingPoolKey, vestingPoolKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), vestingKey.toBuffer(), ], program.programId);
+    const tx = await program.rpc.depositVesting(
+      vestingAmount,
+      globalStateKeyNonce,
+      vestingKeyNonce,
+      vestingPoolKeyNonce,
+      {
+      accounts: {
+        superOwner: wallet.publicKey,
+        vesting: vestingKey,
+        globalState: globalStateKey,
+        poolVestingToken: vestingPoolKey,
+        userVestingToken: userVestingToken,
+        destinationOwner: destinationOwner.publicKey,
+        mintVestingToken: vestingTokenMint.publicKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID
+      }
+    });
+    console.log('txid = ', tx);
+  });
+  it('Withdraw Vesting', async () => {
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    let [vestingKey, vestingKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), wallet.publicKey.toBuffer(), ], program.programId);
+    let [vestingPoolKey, vestingPoolKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), vestingKey.toBuffer(), ], program.programId);
+    const tx = await program.rpc.withdrawVesting(
+      vestingAmount,
+      globalStateKeyNonce,
+      vestingKeyNonce,
+      vestingPoolKeyNonce,
+      {
+      accounts: {
+        superOwner: wallet.publicKey,
+        vesting: vestingKey,
+        globalState: globalStateKey,
+        poolVestingToken: vestingPoolKey,
+        userVestingToken: userVestingToken,
+        destinationOwner: destinationOwner.publicKey,
+        mintVestingToken: vestingTokenMint.publicKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID
+      }
+    });
+    console.log('txid = ', tx);
+  });
+  let destVestingToken: anchor.web3.PublicKey = null;
+  it('Claim', async () => {
+    destVestingToken = await vestingTokenMint.createAccount(wallet.publicKey);
+    let [globalStateKey, globalStateKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(GLOBAL_STATE_TAG)], program.programId);
+    let [vestingKey, vestingKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), wallet.publicKey.toBuffer(), ], program.programId);
+    let [vestingPoolKey, vestingPoolKeyNonce] = await anchor.web3.PublicKey.findProgramAddress([Buffer.from(VESTING_TAG), vestingKey.toBuffer(), ], program.programId);
+    const tx = await program.rpc.claim(
+      globalStateKeyNonce,
+      vestingKeyNonce,
+      vestingPoolKeyNonce,
+      {
+      accounts: {
+        owner: destinationOwner.publicKey,
+        vesting: vestingKey,
+        globalState: globalStateKey,
+        poolVestingToken: vestingPoolKey,
+        userVestingToken: destVestingToken,
+        destinationOwner: destinationOwner.publicKey,
+        mintVestingToken: vestingTokenMint.publicKey,
+        systemProgram: SYSTEM_PROGRAM_ID,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        rent: RENT_SYSVAR_ID,
+        clock: CLOCK_SYSVAR_ID
+      },
+      signers: [destinationOwner],
+    });
+    console.log('txid = ', tx);
+  });
+
 });
+ 
